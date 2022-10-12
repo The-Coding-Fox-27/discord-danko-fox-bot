e191489b9bd4c9c864b38a56c278513ff793f605
The-Coding-Fox-27 <andresr3279@gmail.com>
2022-10-12 00:39:59 -0400

    testinf ready v2

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 const client = require(path.resolve(__dirname, "../bot")).client
 const {createCommand}=require(path.resolve(__dirname, "../dataHandler"))
+const collectionMaker=require(path.resolve(__dirname, "../collectionMaker"))
 client.on("ready", async () => {
   //basically sets the prescense of the bot
   client.user.setPresence({
@@ -15,6 +16,14 @@ client.on("ready", async () => {
     createCommand(client,i.id)
   }
   )
+  const Channels = client.channels
+  channelList=await collectionMaker.roleMesageModel.find({})
+  channelList.forEach(async (i)=>{
+    targetChannel=await Channels.fetch(i.channelid)
+    await targetChannel.messages.fetch(i.messageid)
+  })
+ 
+  
   
 
 
