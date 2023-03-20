13dfb684bbc9be3ca4d4a17287008c1ed529e396
The-Coding-Fox-27 <andresr3279@gmail.com>
2023-03-19 20:19:54 -0400

    bug fix

--- hornyjailchecker.js
+++ hornyjailchecker.js
@@ -7,29 +7,28 @@ async function hornyjailchecker(Channels){
     const day = new Date(today.getFullYear(), today.getMonth(), today.getDate() )
     
     const tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1)
-    hornyList= await collectionMaker.hornyModel.find({sentence:{
+    let hornyList= await collectionMaker.hornyModel.find({sentence:{
     // $gte: day, 
     $lt: tomorrow}
     })
 
     hornyList.forEach(async(i)=>{
-    targetChannel=await Channels.fetch(i.channelid)
-    targetUser=await targetChannel.guild.members.fetch(i.userid)
-    var searchRole=await targetChannel.guild.roles.cache.find(role=>role.name=="Horny jail")
-    await targetChannel.send(`Okay ${targetUser}, you have served your sentence. You are free from horny jail`)
-    try{
-        await targetUser.roles.remove(searchRole)
-    }catch(err){
-        console.log(err)
-    }
-    await collectionMaker.hornyModel.deleteOne({userid:i.userid})
-    
+        let targetChannel=await Channels.fetch(i.channelid)
+        let targetUser=await targetChannel.guild.members.fetch(i.userid)
+        let searchRole=await targetChannel.guild.roles.cache.find(role=>role.name=="Horny jail")
+        await targetChannel.send(`Okay ${targetUser}, you have served your sentence. You are free from horny jail`)
+        try{
+            await targetUser.roles.remove(searchRole)
+        }catch(err){
+            console.log(err)
+        }
+        await collectionMaker.hornyModel.deleteOne({userid:i.userid})
     })
-    return 0
 }
 if (require.main === module) {
     
     const Channels = client.channels
     hornyjailchecker(Channels)
 }
+
 module.exports.hornyjailchecker=hornyjailchecker
